43c766c9e5b82c00a35f47e3192195c4cd93b350
Hosam Hasan <hosamkora@gmail.com>
2020-02-17 18:59:41 +0200

    remove extra space typo

--- lib/model/movie.dart
+++ lib/model/movie.dart
@@ -5,13 +5,13 @@ part 'movie.g.dart';
 
 @freezed
 abstract class Movie with _$Movie {
-  factory Movie(
+  factory Movie({
     int id,
     String title,
     int year,
     double rating,
-    @JsonKey(name: " medium_cover_image") Uri mediumCoverImage,
-  ) = _Movie;
+    @JsonKey(name: "medium_cover_image") Uri mediumCoverImage,
+  }) = _Movie;
 
   factory Movie.fromJson(Map<String, dynamic> json) => _$MovieFromJson(json);
 }

39a3ee0b769ad3733f22fe79c22bf3dd37117c1b
Hosam Hasan <hosamkora@gmail.com>
2020-02-21 22:52:32 +0200

    bese yts api

--- lib/api/yts_api.dart
+++ lib/api/yts_api.dart
@@ -1,7 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
+import 'package:yts_movies/main.dart';
+import 'package:yts_movies/model/torrent.dart';
+import 'package:yts_movies/model/yts_response.dart';
+import 'package:yts_movies/model/yts_response_data.dart';
 import 'package:yts_movies/service/rest_client.dart';
+import 'package:yts_movies/model/movie.dart';
 
 part 'yts_api.g.dart';
 
@@ -10,4 +15,22 @@ part 'yts_api.g.dart';
 abstract class YtsApi {
   @factoryMethod
   factory YtsApi(RestClient client) => _YtsApi(client.dio);
+
+  @GET('list_movies.json')
+  Future<YtsResponse<MoviesData>> getMovies({
+    @Query('query_term') String queryTerm,
+    @Query('limit') int limit,
+    @Query('page') int page,
+    @Query('quality') String quality,
+    @Query('minimum_rating') int minimumRating,
+    @Query('genre') String genre,
+    @Query('sort_by') String sortBy,
+    @Query('order_by') String orderBy,
+    @Query('with_rt_ratings') bool withRottenTomatoesratings,
+  });
+
+  @GET('movie_details.json')
+  Future<YtsResponse<MovieData>> getMovie(
+    @Query('movie_id') int movieId,
+  );
 }
--- lib/api/yts_api.g.dart
+++ lib/api/yts_api.g.dart
@@ -15,4 +15,61 @@ class _YtsApi implements YtsApi {
   final Dio _dio;
 
   String baseUrl;
+
+  @override
+  getMovies(
+      {queryTerm,
+      limit,
+      page,
+      quality,
+      minimumRating,
+      genre,
+      sortBy,
+      orderBy,
+      withRottenTomatoesratings}) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{
+      'query_term': queryTerm,
+      'limit': limit,
+      'page': page,
+      'quality': quality,
+      'minimum_rating': minimumRating,
+      'genre': genre,
+      'sort_by': sortBy,
+      'order_by': orderBy,
+      'with_rt_ratings': withRottenTomatoesratings
+    };
+    queryParameters.removeWhere((k, v) => v == null);
+    final _data = <String, dynamic>{};
+    final Response<Map<String, dynamic>> _result = await _dio.request(
+        'list_movies.json',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'GET',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = YtsResponse<MoviesData>.fromJson(_result.data);
+    return Future.value(value);
+  }
+
+  @override
+  getMovie(movieId) async {
+    ArgumentError.checkNotNull(movieId, 'movieId');
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{'movie_id': movieId};
+    final _data = <String, dynamic>{};
+    final Response<Map<String, dynamic>> _result = await _dio.request(
+        'movie_details.json',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'GET',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = YtsResponse<MovieData>.fromJson(_result.data);
+    return Future.value(value);
+  }
 }

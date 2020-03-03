a0d9e235acf50b74ce68f719b5d8375fb6ef4b5f
Hosam Hasan <hosamkora@gmail.com>
2020-03-03 13:48:50 +0200

    movies page base

---
+++ lib/ui/pages/movies_page/movie_list_element.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:smooth_star_rating/smooth_star_rating.dart';
+import 'package:yts_movies/model/movie.dart';
+import 'package:yts_movies/util/size_extensions.dart';
+import 'package:yts_movies/model/quality.dart';
+import 'package:yts_movies/model/genre.dart';
+
+class MovieListElement extends StatelessWidget {
+  final Movie movie;
+  const MovieListElement(this.movie, {key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Image.network(
+            movie.mediumCoverImage.toString(),
+            height: 400.w,
+          ),
+          SizedBox(width: 20.w),
+          Expanded(
+            child: Container(
+              // decoration: border,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Text(
+                    movie.title,
+                    maxLines: 3,
+                    style: TextStyle(
+                      fontSize: 50.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    movie.year.toString(),
+                    style: TextStyle(fontSize: 40.sp),
+                    maxLines: 3,
+                  ),
+                  SmoothStarRating(
+                    size: 65.w,
+                    allowHalfRating: true,
+                    rating: movie.rating / 2,
+                    color: Colors.cyan,
+                    borderColor: Colors.cyan,
+                    spacing: 5.w,
+                  ),
+                  Wrap(
+                    spacing: 15.w,
+                    runSpacing: 15.h,
+                    children: movie.torrents
+                        .map((torrent) => DecoratedBox(
+                              decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: Colors.cyan, width: 1),
+                                borderRadius: BorderRadius.circular(40),
+                              ),
+                              child: Padding(
+                                  padding: EdgeInsets.all(5),
+                                  child: Text(
+                                    "${torrent.quality.value} ${torrent.type.value}",
+                                    style: TextStyle(fontSize: 40.sp),
+                                  )),
+                            ))
+                        .toList(),
+                  ),
+                  Text(
+                    movie.genres.map((genre) => genre.value).join(' , '),
+                    style: TextStyle(fontSize: 40.sp),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/ui/pages/movies_page/movies_page.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:yts_movies/model/movie.dart';
+import 'package:yts_movies/model/quality.dart';
+import 'package:yts_movies/repository/movies_repository.dart';
+import 'package:yts_movies/service/get_it_config.dart';
+import 'package:yts_movies/ui/pages/movies_page/movie_list_element.dart';
+import 'package:yts_movies/util/result.dart';
+import 'package:yts_movies/view_models/movies_store.dart';
+import 'package:yts_movies/util/object_extensions.dart';
+import 'package:yts_movies/util/size_extensions.dart';
+
+class MoviesPage extends StatefulWidget {
+  MoviesPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MoviesPageState createState() => _MoviesPageState();
+}
+
+class _MoviesPageState extends State<MoviesPage> {
+  final model = getIt.get<MoviesStore>();
+  ScrollController controller = ScrollController();
+  final isBottomEdge = false.listenable;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size(double.infinity, 150.h),
+          child: AppBar(
+            centerTitle: true,
+            title: Text(
+              widget.title,
+              style: TextStyle(fontSize: 55.sp),
+            ),
+          ),
+        ),
+        body: FutureBuilder<Result<List<Movie>>>(
+          future: getIt.get<MoviesRepository>().getMovies(
+                limit: 50,
+                sortBy: 'year',
+                quality: Quality.UHD,
+              ),
+          builder: (_, snapshot) {
+            print(snapshot.data);
+            return snapshot.data == null
+                ? Center(child: CircularProgressIndicator())
+                : snapshot.data.when<Widget>(
+                    error: (error) => Center(
+                      child: Text(error),
+                    ),
+                    payload: (movies) =>
+                        NotificationListener<ScrollUpdateNotification>(
+                      onNotification: (notification) {
+                        if (controller.hasClients &&
+                            controller.position.atEdge &&
+                            controller.position.userScrollDirection ==
+                                ScrollDirection.reverse)
+                          isBottomEdge.value = true;
+                        else
+                          isBottomEdge.value = false;
+                      },
+                      child: Column(
+                        children: <Widget>[
+                          Expanded(
+                            child: ListView(
+                              controller: controller,
+                              children: [
+                                ...movies
+                                    .map(
+                                      (movie) => InkWell(
+                                        onTap: () {},
+                                        child: MovieListElement(movie),
+                                      ),
+                                    )
+                                    .toList(),
+                                ValueListenableBuilder<bool>(
+                                    valueListenable: isBottomEdge,
+                                    builder: (_, value, __) => value
+                                        ? Center(
+                                            child: CircularProgressIndicator())
+                                        : SizedBox.shrink())
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class BorderdContainer extends StatelessWidget {
+  final child;
+  const BorderdContainer({Key key, this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+      decoration: BoxDecoration(border: Border.all(color: Colors.cyan)),
+    );
+  }
+}
